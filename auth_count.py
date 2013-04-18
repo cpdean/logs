5ccb5c1419e93b65ff84d633b585f3d30ce41797
Conrad Dean <conrad.p.dean@gmail.com>
2013-04-17 20:07:11 -0500

    opening gzip is easy and fast

--- auth_count.py
+++ auth_count.py
@@ -1,10 +1,15 @@
-
 import os
+from glob import glob
+import gzip
 
 mainlogs = "/var/log/"
 subject = "auth.log"
 
 log_path = os.path.join(mainlogs, subject)
 
-with open(log_path, "rb") as f:
+def log_files(log_file):
+    """list of rotated log files"""
+    return glob(log_file + "*")
+
+with gzip.open(log_path + ".2.gz") as f:
     print len(f.readlines())
